cc0f49eb4eff77430595788fc4f35eded2845799
Rafal Wilinski <43538102+Rafal-WIlinski@users.noreply.github.com>
2023-10-23 17:48:14 +0200

    init

--- src/js/Message.js
+++ src/js/Message.js
@@ -4,7 +4,7 @@ const Message = () => {
   return (
     <div className="content">
       <h1>Rexpack</h1>
-      <p className="description">React, Express, and Webpack Boilerplate Application</p>
+      <p className="description">React, Express, and Webpack Boilerplate Application edited  :))) </p>
       <div className="awful-selfie"></div>
     </div>
   )
